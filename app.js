9ad9adb384b76484fc385d3bc79911912fbce743
Oliver Kiessler <kiessler@inceedo.com>
2012-06-23 20:25:45 +0200

    Redis improvements / Bugfixes

--- app.js
+++ app.js
@@ -3,28 +3,39 @@ var fs    = require('fs'),
 var http = require('http'),
     https = require('https'),
     httpProxy = require('http-proxy');
-    routing = require('./redis_routing_table');
+var debugging = true;
+var routing = require('./redis_routing_table');
 var routingTable = {};
-nconf.argv().env().file({ file: 'config.json' });
-var debugging = nconf.get('debugging');
 
 function log(message) {
   if (debugging) console.log(message);
 }
 
-routing.loadRoutingTable(nconf, routingTable);
-
-httpProxy.createServer(function (req, res, proxy) {
-  var buffer = httpProxy.buffer(req);
-  var destinationHost = routingTable[req.headers.host]
-  log("\n" + req.url);
-  log(req.headers.host + " => " + destinationHost);
-  
-  if (destinationHost != null) {
-    proxy.proxyRequest(req, res, {
-      host: destinationHost,
-      port: 80,
-      buffer: buffer
-    });
+nconf.file({ file: 'config.json' }).load(function(err) {
+  if (!err) {
+    start();
+  } else {
+    console.log("Couldn't load config.json");
   }
-}).listen(process.env.VMC_APP_PORT || process.env.PORT || 8000);
+});
+
+function start() {
+  debugging = nconf.get('debugging');
+  routing.loadRoutingTable(nconf, routingTable);
+
+  httpProxy.createServer(function (req, res, proxy) {
+    var buffer = httpProxy.buffer(req);
+    var destinationHost = routingTable[req.headers.host];
+    log(req.url);
+    // log(req.headers.host + " => " + destinationHost);
+    req.headers.host = destinationHost;
+
+    if (destinationHost != null) {
+      proxy.proxyRequest(req, res, {
+        host: destinationHost,
+        port: 80,
+        buffer: buffer
+      });
+    }
+  }).listen(80);
+}
--- redis_routing_table.js
+++ redis_routing_table.js
@@ -3,6 +3,9 @@ var redis = require("redis");
 exports.loadRoutingTable = function(nconf, redisRoutingTable) {
   console.log("Loading routing table...");
 
+  console.log("Redis Host: " + nconf.get('host'));
+  console.log("Redis Port: " + nconf.get('port'));
+
   client = redis.createClient(nconf.get('port'), nconf.get('host'), { no_ready_check: true });
   client.on("error", function (err) {
     console.log(err);
@@ -12,14 +15,17 @@ exports.loadRoutingTable = function(nconf, redisRoutingTable) {
     if (err) {
       console.log("Redis connection failed!");
     } else {
+      console.log("Connected to redis...");
       client.select(nconf.get('dbindex'), function(err, res) {
     	  if (!err) {
+    	    console.log("Readings hosts...");
     	    client.llen('hosts', function(err, length) {
             if (!err) {
               client.lrange('hosts', 0, length, function(err, hosts) {
                 for (var i = 0; i < hosts.length; i++) {
                   getHost = function(client, host, redisRoutingTable) {
                     client.hget(host, 'host', function(err, dest) {
+                      console.log(host + " => " + dest);
                       redisRoutingTable[host] = dest;
                     });
                   }
@@ -28,6 +34,8 @@ exports.loadRoutingTable = function(nconf, redisRoutingTable) {
               });
             }
           });
+        } else {
+          console.log("Error: " + err);
         }
       });
     }
